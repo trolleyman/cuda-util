ff3f14257816cabc8b646ce6557954cb6ee44663
Callum Tolley <cgtrolley@gmail.com>
2019-07-21 16:45:27 +0100

    Add tests to cuda-macros-util

--- cuda-macros-util/src/lib.rs
+++ cuda-macros-util/src/lib.rs
@@ -1,4 +1,9 @@
 
+extern crate cuda;
+extern crate proc_macro2;
+extern crate syn;
+extern crate quote;
+
 
 /// Checks if an identifier has the value given
 pub fn ident_eq(ident: &syn::Ident, value: &str) -> bool {
@@ -6,35 +11,37 @@ pub fn ident_eq(ident: &syn::Ident, value: &str) -> bool {
 }
 
 /// Checks a type path and a string that represents a type path, to see if they match
-pub fn type_path_matches(type_path: &syn::TypePath, mut ty: &str) -> bool {
+pub fn type_path_matches(path: &syn::Path, mut ty: &str) -> bool {
 	if ty.starts_with("::") {
 		ty = &ty["::".len()..];
 	}
 	let segment_count = ty.split("::").count();
-	if type_path.path.leading_colon.is_some() {
+	if path.leading_colon.is_some() {
 		// Have to match fully
-		if segment_count != type_path.path.segments.len() {
+		if segment_count != path.segments.len() {
 			return false;
 		}
 		for (i, segment) in ty.split("::").enumerate() {
-			if i >= type_path.path.segments.len() {
+			if i >= path.segments.len() {
 				break;
 			}
-			if !ident_eq(&type_path.path.segments[i].ident, segment)
-					|| !type_path.path.segments[i].arguments.is_empty() {
+			if !ident_eq(&path.segments[i].ident, segment)
+					|| !path.segments[i].arguments.is_empty() {
 				return false;
 			}
 		}
 		true
 	} else {
 		// Can match in any place
-		for (i, segment) in ty.rsplit("::").enumerate() {
-			let j: isize = type_path.path.segments.len() as isize - i as isize - 1;
-			if j <= 0 {
-				break;
-			}
-			if !ident_eq(&type_path.path.segments[i].ident, segment)
-					|| !type_path.path.segments[i].arguments.is_empty() {
+		let mut ty_it = ty.rsplit("::");
+		for i in (0..path.segments.len()).rev() {
+			let segment = match ty_it.next() {
+				Some(s) => s,
+				None => return false,
+			};
+			// println!("{}, {}: {}", i, &segment, &path.segments[i].ident);
+			if !ident_eq(&path.segments[i].ident, segment)
+					|| !path.segments[i].arguments.is_empty() {
 				return false;
 			}
 		}
@@ -43,7 +50,7 @@ pub fn type_path_matches(type_path: &syn::TypePath, mut ty: &str) -> bool {
 }
 
 
-#[derive(Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum FunctionType {
 	Host,
 	Device,
@@ -51,32 +58,64 @@ pub enum FunctionType {
 }
 impl FunctionType {
 	pub fn try_from_attr(attr: &syn::Attribute) -> Option<Self> {
-		fn parse_ident(ident: &syn::Ident) -> Option<FunctionType> {
-			if ident_eq(ident, "host") {
-				Some(FunctionType::Host)
-			} else if ident_eq(ident, "device") {
-				Some(FunctionType::Device)
-			} else if ident_eq(ident, "global") {
-				Some(FunctionType::Global)
-			} else {
-				None
+		Self::try_from_path(&attr.path)
+	}
+
+	pub fn try_from_path(path: &syn::Path) -> Option<Self> {
+		use crate::FunctionType::*;
+
+		for (name, &ty) in ["host", "device", "global"].iter().zip([Host, Device, Global].iter()) {
+			if crate::type_path_matches(&path, &format!("::cuda_macros_impl::{}", name)) {
+				return Some(ty);
+			} else if crate::type_path_matches(&path, &format!("::cuda_macros::{}", name)) {
+				return Some(ty);
 			}
 		}
-		if attr.path.leading_colon.is_some() {
-			// Match path exactly
-			if attr.path.segments.len() == 2
-					&& ident_eq(&attr.path.segments[0].ident, "cuda-macros-impl") {
-				parse_ident(&attr.path.segments[1].ident)
-			} else {
-				None
-			}
-		} else if attr.path.segments.len() == 1 {
-			parse_ident(&attr.path.segments[0].ident)
-		} else if attr.path.segments.len() == 2
-				&& ident_eq(&attr.path.segments[0].ident, "cuda-macros-impl") {
-			parse_ident(&attr.path.segments[1].ident)
-		} else {
-			None
-		}
+		None
+	}
+}
+
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_ident_eq() {
+		assert!(ident_eq(&syn::parse_str::<syn::Ident>("i32").unwrap(), "i32"));
+		assert!(!ident_eq(&syn::parse_str::<syn::Ident>("i32").unwrap(), "f32"));
+	}
+
+	#[test]
+	fn test_type_path_matches() {
+		assert!(type_path_matches(&syn::parse_str("::std::os::raw::c_long").unwrap(), "::std::os::raw::c_long"));
+		assert!(type_path_matches(&syn::parse_str("std::os::raw::c_long").unwrap(), "::std::os::raw::c_long"));
+		assert!(type_path_matches(&syn::parse_str("os::raw::c_long").unwrap(), "::std::os::raw::c_long"));
+		assert!(type_path_matches(&syn::parse_str("raw::c_long").unwrap(), "::std::os::raw::c_long"));
+		assert!(type_path_matches(&syn::parse_str("c_long").unwrap(), "::std::os::raw::c_long"));
+		assert!(type_path_matches(&syn::parse_str("i32").unwrap(), "i32"));
+		
+		assert!(!type_path_matches(&syn::parse_str("::std::os::raw::c_long").unwrap(), "c_long"));
+		assert!(!type_path_matches(&syn::parse_str("std::os::raw::c_long").unwrap(), "c_long"));
+		assert!(!type_path_matches(&syn::parse_str("os::raw::c_long").unwrap(), "c_long"));
+		assert!(!type_path_matches(&syn::parse_str("raw::c_long").unwrap(), "c_long"));
+		assert!(type_path_matches(&syn::parse_str("c_long").unwrap(), "c_long"));
+		
+		assert!(!type_path_matches(&syn::parse_str("std::os::raw::c_long").unwrap(), "::std::unix::raw::c_long"));
+		assert!(!type_path_matches(&syn::parse_str("something::std::os::raw::c_long").unwrap(), "::std::os::raw::c_long"));
+	}
+	
+	#[test]
+	fn test_function_type_try_from_path() {
+		use crate::FunctionType::*;
+
+		assert_eq!(Some(Host)  , FunctionType::try_from_path(&syn::parse_str("host"  ).unwrap()));
+		assert_eq!(Some(Device), FunctionType::try_from_path(&syn::parse_str("device").unwrap()));
+		assert_eq!(Some(Global), FunctionType::try_from_path(&syn::parse_str("global").unwrap()));
+
+		assert_eq!(Some(Host), FunctionType::try_from_path(&syn::parse_str("::cuda_macros::host").unwrap()));
+		assert_eq!(Some(Host), FunctionType::try_from_path(&syn::parse_str("cuda_macros::host").unwrap()));
+		assert_eq!(Some(Host), FunctionType::try_from_path(&syn::parse_str("::cuda_macros_impl::host").unwrap()));
+		assert_eq!(Some(Host), FunctionType::try_from_path(&syn::parse_str("cuda_macros_impl::host").unwrap()));
 	}
 }
